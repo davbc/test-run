b48f946196301d246144ddbf06000a644b2f45c0
davbc <adeoluwado@gmail.com>
2022-07-29 10:25:29 -0400

    commit

---
+++ this.py
@@ -0,0 +1,17 @@
+#print ("hello world, ben is a useless boy")
+d=15
+c=30
+
+a=10
+b=20
+
+if a > b:
+    print("david is stupid")
+else:
+    print ("david is a warey!!!!!!!!")
+
+
+a=[1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1]
+b=[' '*2*(9-i)+'davi'*i for i in a]
+for line in b:
+    print (line)
